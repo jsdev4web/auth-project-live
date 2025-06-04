a0ed3ff295f22c6651c94526a684cb8c2169560d
jsweb4dev <jsweb4dev@gmail.com>
2025-06-04 18:12:02 -0400

    live project build

--- db/queries.js
+++ db/queries.js
@@ -37,7 +37,7 @@ async function getAllMessages() {
 
 
 async function getMessages() {
-  const { rows } = await pool.query("SELECT msg.id, msg.firstname, msg.message FROM messages INNER JOIN signups ON msg.email = signups.email;");
+  const { rows } = await pool.query("SELECT msg.id, msg.firstname, msg.message FROM messages msg INNER JOIN signups ON msg.email = signups.email;");
   return rows;
 }
 
